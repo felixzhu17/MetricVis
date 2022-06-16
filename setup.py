6b1a24d835b6cfbea9e8a0d8bc61f9e4ca055fab
Felix Zhu <fzhu@atlassian.com>
2022-06-16 20:56:29 +1000

    .

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="metric-vis",
+    version="0.0.2",
+    license="MIT",
+    author="Felix Zhu",
+    author_email="zhu.felix@outlook.com",
+    description="Metric Visualisation",
+    long_description=long_description,
+    packages=find_packages(),
+    setup_requires=["setuptools_scm"],
+    url="https://github.com/felixzhu17/MetricVis",
+    install_requires=[
+        "plotly",
+        "plotly-express",
+        "pandas",
+        "numpy",
+        "nbformat",
+    ],
+)
